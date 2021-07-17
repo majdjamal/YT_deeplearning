0801a390373339c747f3d49f19b53fa6f11d6445
majdjamal <majdj@kth.se>
2021-07-17 21:06:49 +0200

    added comments

--- training.py
+++ training.py
@@ -11,7 +11,10 @@ def loadData():
 	return x_train, y_train, x_test, y_test
 
 def model(dim = (32, 32, 3), Nclasses = 10):
-	""" Creates and compiles a deep learning model. 
+	""" Creates and compiles a deep learning model.
+	:param dim: Dimension of input data
+	:param Nclasses: Number of unqiue classes
+	:return net: Built and compiled MobileNet
 	"""
 	net = app.MobileNet(include_top=True,
 						    weights=None,
@@ -29,14 +32,22 @@ def model(dim = (32, 32, 3), Nclasses = 10):
 
 def train(model, X_train, y_train):
 	""" Trains the model.
+	:param model: Tensorflow network
+	:param X_train: Training patterns, shape = (Npts, 32,32,3)
+	:param y_train: Training labels, shape = (Npts, 1)
+	:return model: trained network
 	"""
-	model.fit(X_train, y_train, epochs=20, 
+	model.fit(X_train, y_train, epochs=20,
                     validation_split = 0.3)
 
 	return model
 
 def evaluate(model, X_test, y_test):
 	"""	Evaluates the model
+	:param model: Tensorflow network
+	:param X_test: Test patterns, shape = (Npts, 32,32,3)
+	:param y_test: Test labels, shape = (Npts, 1)
+	:return test_acc: Accuracy on testing set
 	"""
 	test_loss, test_acc = model.evaluate(X_test,  y_test)
 	return test_acc
